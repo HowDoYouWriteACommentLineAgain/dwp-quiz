adfbee68a71f024cf011eeb2551eb1282561d0a4
Awsmin <103874344+HowDoYouWriteACommentLineAgain@users.noreply.github.com>
2025-05-23 22:34:30 +0800

    second commit of my changes

---
+++ resources/views/games/show.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>Games Show</h1>
+        <li style="color: rgb(36, 18, 101)"><b><i>ID: {{ $games['id'] }}</i></b></li>
+        <li><b>{{ $games['title'] }}</b></li>
+        <li><i>{{ $games['developer'] }}</i></li>
+    <ul>
+</div>
